462d21a2a1a8ee8fb7b7cb4d7641fabab114b24b
zoo868e <zoo868e@gmail.com>
2020-08-22 23:13:54 +0800

    Accept 10200
    Solve TLE by compute all posible number of i and save in an array.
    So everytime get input,only need to add the num[a] to num[b].
    And also solve the problem of accuracy by add EPS.

--- 10200/main.c
+++ 10200/main.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-int prime[10000];
+#define EPS 1e-8
 
-void generate();
 int isprime(int x);
 
 
 int main(){
-	generate();
-    int temp,ft;
+    int temp,ft,num[10010];
     int ans,count,a,b;
+    memset(num,0,sizeof(num));
+    for(ft = 0;ft <= 10001;ft++)
+        num[ft] = isprime(ft*ft+ft+41);
     while(scanf("%d %d",&a,&b)!=EOF){
         count = 0;
-        ans = b - a + 1;
-        for(ft = a;ft <= b;ft++){
-            temp = ft*ft + ft + 41;
-            if(isprime(temp))count++;
-        }
-        printf("%.2f\n",count*100.0/ans);
+        for(ft = a;ft <= b;ft++)
+            count+=num[ft];
+        printf("%.2f\n",count*1.0/(b-a+1)*100+EPS);
     }
     return 0;
-}
-void generate()
-{
-	memset(prime,0,sizeof(prime));
-	int temp = 0,i;
-	for(i = 2;i < 46340;i++){
-		if(isprime(i)){
-			prime[temp] = i;
-			temp++;
-}
-}
 }
 
 int isprime(int x){
 	int i;
-	for(i = 0;i < 10000;i++){
-		if((x > (prime[i] * prime[i])) && ((x % prime[i]) == 0))return 0;
+	for(i = 2;i*i <= x;i++){
+        if(x % i);
+        else return 0;
 }
 	return 1;
 }
