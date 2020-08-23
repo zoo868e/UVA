21a2a13ab229e46ce9321be91e8feb97273ca37a
zoo868e <zoo868e@gmail.com>
2020-08-23 20:21:13 +0800

    Complete the problem
    Refer to 小白菜又菜's dfs approach.

--- 10098/main.c
+++ 10098/main.c
@@ -1,30 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 
-void swap();
+char data[11];
+char save[3628800][11];
+char temp[11];
+int  used[11];
+ 
+int  Count;
 
-int main(){
-    int n,wp,fp;
-    char a[10];
-    scanf("%d",&n);
-    while(n > 0){
-        wp = 0;
-        gets(a);
-        while(a[wp]!=NULL){
-            for(fp = 0;fp < wp;fp++){
-                if(a[wp] < a[fp])swap(a[wp],a[fp]);
-            }
-            wp++;
-        }
-
-
-        n--;
-    }
-    return 0;
-}
-
-void swap(int *a,int *b)
+void swap(char *a,char *b)
 {
     *a = *a ^ *b;
     *b = *a ^ *b;
     *a = *a ^ *b;
+}
+
+void dfs( int d, int n )
+{
+	int flag = 1,i;
+	for ( i = 0 ; i < d ; ++ i )
+		if ( save[Count-1][i] != temp[i] ) {
+			flag = 0;break;
+		}
+	if ( d && flag ) return;
+	
+	if ( d == n ) {
+		for ( i = 0 ; i < n ; ++ i )
+			save[Count][i] = temp[i];
+		save[Count ++][n] = 0;
+	}
+	for ( i = 0 ; i < n ; ++ i )
+		if ( !used[i] ) {
+			used[i] = 1;
+			temp[d] = data[i];
+			dfs( d+1, n );
+			used[i] = 0;
+		}
+}
+int main(){
+    int n,wp,fp;
+    scanf("%d",&n);
+    while(n--){
+        wp = 0;
+        scanf("%s",data);
+        while(data[wp]){
+            for(fp = 0;fp < wp; fp++){
+                if(data[wp] < data[fp])swap(&data[wp],&data[fp]);
+            }
+            wp++;
+        }
+        Count = 1;
+		memset(save,0,sizeof(save));
+        dfs(0,strlen(data));
+		for(fp = 1;fp < Count;fp++)
+			printf("%s\n",save[fp]);
+        printf("\n");
+}
+    return 0;
 }
