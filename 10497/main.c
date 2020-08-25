4575c5d591b084306275fa9a72d9e47e6b5ea0d6
zoo868e <zoo868e@gmail.com>
2020-08-25 22:21:24 +0800

    Complete
    Don't need to implement the big number times big number.
    Only need to spilit the answer into array.

--- 10497/main.c
+++ 10497/main.c
@@ -2,43 +2,61 @@
 #include<string.h>
 #include<math.h>
 
-int dp[801][500];
+int dp[801][1000];
 
 void gendp();
+void carry();
+void show();
 
 int main(){
-    unsigned long long dn[801];
-    int n,fp;
-    memset(dn,0,sizeof(dn));
-    dn[1] = 0;
-    for(fp = 2;fp <= 800;fp++)
-        dn[fp] = fp*dn[fp-1] + pow(-1,fp);
-    scanf("%d",&n);
-    while(n != -1){
-        printf("%llu\n",dn[n]);
-        scanf("%d",&n);
-    }
+	gendp();
+	int n,fp;
+	scanf("%d",&n);
+	while(n != -1){
+		show(n);
+		scanf("%d",&n);
+}
     return 0;
 }
 
 void gendp()
 {
-	memset(dp,0,sizeof(dp));
+	memset(dp,-1,sizeof(dp));
 	int fp1,fp2;
+	dp[1][0] = 0;
 	for(fp1 = 2;fp1 <= 800;fp1++){
-		for(fp2 = 0;fp2 < 500;fp2++){
-			dp[fp1][fp2] = fp1*dp[fp1-1][fp2];
+		for(fp2 = 0;fp2 < 1000;fp2++){
+			if(dp[fp1-1][fp2] >= 0)
+				dp[fp1][fp2] = fp1*dp[fp1-1][fp2];
 		}
 		dp[fp1][0] += pow(-1,fp1);
+		for(fp2 = 0;(fp2 < 1000) && (dp[fp1][fp2] >= 0);fp2++){
+			carry(fp1,fp2);
+			if(dp[fp1][fp2] == -1)break;
+		}
 	}
 
 }
 
 void carry(int x,int i)
 {
-	if(dp[i] > 1000){
-		dp[x][i+1] = dp[x][i]/1000;
+	if(dp[x][i] >= 1000){
+		if(dp[x][i+1] == -1)dp[x][i+1] = 0;
+		dp[x][i+1] += dp[x][i]/1000;
 		dp[x][i] = dp[x][i]%1000;
-		carry(i+1);
 	}
+}
+
+
+void show(int x)
+{
+	int fp;
+	for(fp = 999; fp >=0; fp--){
+		if(dp[x][fp] == -1)continue;
+		if(dp[x][fp+1] == -1) printf("%d",dp[x][fp]);
+		else if((dp[x][fp] < 100) && (dp[x][fp] > 9))printf("0%d",dp[x][fp]);
+		else if(dp[x][fp] < 10)printf("00%d",dp[x][fp]);
+		else printf("%d",dp[x][fp]);
+	}
+	printf("\n");
 }
