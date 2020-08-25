af32da3ec79cf309711472e97102fe43709308e3
zoo868e <zoo868e@gmail.com>
2020-08-26 07:22:33 +0800

    Haven't complete
    Sort the forans array so n % i is the number what I want.
    Still have bug and I haven't figure out where is it.

--- 10515/main.c
+++ 10515/main.c
@@ -5,6 +5,7 @@ void ans();
 int check();
 int checkre();
 void genans();
+void swap();
 
 char n[110],m[110];
 
@@ -14,7 +15,7 @@ int forans[10][10],ansnum[10];
 int main(){
 	int fp,ffp;
 	genans();
-	for(fp = 0;fp<10;fp++)printf("ansnum[%d] = %d\n",fp,ansnum[fp]);
+	for(fp = 0;fp < 10;fp ++)for(ffp = 0;ffp < 10;ffp++)printf("forans[%d][%d] = %d\n",fp,ffp,forans[fp][ffp]);
 	scanf("%s %s",m,n);
 	while(check(n,m)){
 		ans();
@@ -27,8 +28,8 @@ void ans()
 {
 	int fp,a = m[strlen(m)-1]-'0';
 	int i = ansnum[a];
-	int x = n[strlen(n)-1]-'0' + (n[strlen(n)-2]-'0')*10;
-	printf("%d\n",forans[a][(x+i-1)%i]);
+	int x = n[strlen(n)-1]-'0' + (n[strlen(n)-2]-'0')*10 + (n[strlen(n)-3]-'0')*100;
+	printf("%d\n",forans[a][x%i]);
 }
 
 int check(char n[],char m[])
@@ -50,14 +51,16 @@ int checkre(int temp,int a)
 void genans()
 {
 	int fp,i = 1,temp,wp;
-	memset(ansnum,1,sizeof(ansnum));
+	memset(ansnum,0,sizeof(ansnum));
 	memset(forans,-1,sizeof(forans));
 	forans[0][0] = 0;
+	ansnum[0] = 1;
 	forans[1][0] = 1;
 	for(fp = 1;fp < 10;fp++){
 		temp = fp;
 		wp = 0;
 		forans[fp][wp] = temp;
+		ansnum[fp] = 1;
 		while(1){
 			temp = temp * fp;
 			temp = temp % 10;
@@ -67,5 +70,15 @@ void genans()
 			}
 			else break;
 		}
+		for(;wp > 0;wp--){
+			swap(&forans[fp][wp],&forans[fp][wp-1]);
+		}
 	}
+}
+
+void swap(int *a,int *b)
+{
+	*a = *a ^ *b;
+	*b = *a ^ *b;
+	*a = *a ^ *b;
 }
