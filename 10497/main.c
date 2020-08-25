d4b06a17fa9f006a4bcdd9e9d0d51477d0e2c4b4
zoo868e <zoo868e@gmail.com>
2020-08-25 08:07:56 +0800

    Haven't complete
    Trying caculate big number by use array.
    dp[number][digit==500]
    Every digit save integer less than 1000.
    But the problem is how to caculate the splitted number times splitted number.

--- 10497/main.c
+++ 10497/main.c
@@ -2,6 +2,10 @@
 #include<string.h>
 #include<math.h>
 
+int dp[801][500];
+
+void gendp();
+
 int main(){
     unsigned long long dn[801];
     int n,fp;
@@ -15,4 +19,26 @@ int main(){
         scanf("%d",&n);
     }
     return 0;
+}
+
+void gendp()
+{
+	memset(dp,0,sizeof(dp));
+	int fp1,fp2;
+	for(fp1 = 2;fp1 <= 800;fp1++){
+		for(fp2 = 0;fp2 < 500;fp2++){
+			dp[fp1][fp2] = fp1*dp[fp1-1][fp2];
+		}
+		dp[fp1][0] += pow(-1,fp1);
+	}
+
+}
+
+void carry(int x,int i)
+{
+	if(dp[i] > 1000){
+		dp[x][i+1] = dp[x][i]/1000;
+		dp[x][i] = dp[x][i]%1000;
+		carry(i+1);
+	}
 }
